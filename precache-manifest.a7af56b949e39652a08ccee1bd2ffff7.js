e07c0f01c2730e4bc4e6834d8996120ba92f582b
leixiaopengxiha <leixiaopengxiha@163.com>
2019-07-30 16:35:34 +0800

    后台管理系统

---
+++ precache-manifest.a7af56b949e39652a08ccee1bd2ffff7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3cf808d67ff6f7e54a467975c7fea67",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bb73f26c7e34ebe543d0",
+    "url": "./static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "74d66d8f05ca290d0353",
+    "url": "./static/css/main.d9537238.chunk.css"
+  },
+  {
+    "revision": "bb73f26c7e34ebe543d0",
+    "url": "./static/js/2.ab0049f7.chunk.js"
+  },
+  {
+    "revision": "74d66d8f05ca290d0353",
+    "url": "./static/js/main.b165f4e8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "277ec3b80ec1ec5f57734209115bacb5",
+    "url": "./static/media/top.277ec3b8.jpg"
+  }
+]);
